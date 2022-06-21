2e02d9f6d5510e9a300a3b035e4e5f6f329d76bb
avagyansamvel <avagyansamvel07@gmail.com>
2022-06-21 16:13:11 +0400

    master

--- globalitAPI/apiproject/apiproject/settings.py
+++ globalitAPI/apiproject/apiproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'globalitc.wsgi.application'
+# wWSGI_APPLICATION = 'apiproject.wsgi.application'
 
 
 # Database
